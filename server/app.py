94a16cb3bdc4ceeaa251a0d890b0022f3efe81b0
Davit Tabidze <davidtabidzef1@gmail.com>
2023-08-02 11:44:36 -0400

    complete time

--- server/app.py
+++ server/app.py
@@ -1,25 +1,103 @@
 #!/usr/bin/env python3
 
-from models import db, Student,Tutor,Session
+from models import db, Student, Tutor, Session
 from flask_restful import Api, Resource
 from flask_migrate import Migrate
 from flask import Flask, make_response, jsonify, request
+from sqlalchemy.exc import IntegrityError
+import datetime
 
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get(
-    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-if __name__ == '__main__':
+
+@app.route("/students", methods=["GET", "POST"])
+def get_students():
+    if request.method == "GET":
+        all = Student.query.all()
+        students = []
+        for student in all:
+            students.append(student.to_dict(rules=("-sessions",)))
+        return students, 200
+    elif request.method == "POST":
+        data = request.json
+        student = Student()
+        try:
+            for attr in data:
+                setattr(student, attr, data[attr])
+            print(student)
+            db.session.add(student)
+            db.session.commit()
+            return (student.to_dict(rules=("-sessions",)), 201)
+        except (IntegrityError, ValueError) as ie:
+            return {"errors": ie.args}, 422
+
+
+@app.route("/students/<int:id>", methods=["GET", "PATCH", "DELETE"])
+def get_Student_by_id(id):
+    student = Student.query.filter(Student.id == id).first()
+    if not student:
+        return {"error": "Student not found"}, 404
+    elif request.method == "GET":
+        return student.to_dict(), 200
+    elif request.method == "PATCH":
+        data = request.json
+        try:
+            for attr in data:
+                setattr(student, attr, data[attr])
+            db.session.commit()
+            return (student.to_dict(rules=("-sessions",)), 200)
+        except (IndentationError, ValueError) as ie:
+            return {"errors": ie.args}, 422
+    elif request.method == "DELETE":
+        db.session.delete(student)
+        db.session.commit()
+        return {}, 200
+
+
+@app.route("/tutors", methods=["GET", "POST"])
+def get_tutors():
+    if request.method == "GET":
+        all = Tutor.query.all()
+        tutors = []
+        for tutor in all:
+            tutors.append(tutor.to_dict(rules=("-sessions",)))
+        return tutors, 200
+
+
+@app.route("/sessions", methods=["POST"])
+def add_session():
+    if request.method == "POST":
+        data = request.json
+        session = Session()
+        try:
+            for attr in data:
+                if attr == "datetime":
+                    setattr(
+                        session,
+                        attr,
+                        datetime.datetime.strptime(data[attr], "%Y-%m-%d %H:%M:%S.%f"),
+                    )
+                else:
+                    setattr(session, attr, data[attr])
+            db.session.add(session)
+            db.session.commit()
+            return session.to_dict(), 201
+        except (IntegrityError, ValueError) as ie:
+            return {"errors": ie.args}, 422
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, func
+import datetime
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
@@ -9,7 +10,7 @@ convention = {
     "uq": "uq_%(table_name)s_%(column_0_name)s",
     "ck": "ck_%(table_name)s_%(constraint_name)s",
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
+    "pk": "pk_%(table_name)s",
 }
 
 metadata = MetaData(naming_convention=convention)
@@ -18,40 +19,86 @@ db = SQLAlchemy(metadata=metadata)
 
 
 class Student(db.Model, SerializerMixin):
-    __tablename__ = 'students'
+    __tablename__ = "students"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
     # Add relationships
-
+    sessions = db.relationship("Session", back_populates="student")
     # Add serialization rules
+    serialize_rules = ("-sessions.student",)
 
-    #add validations
+    # add validations
+    @validates("name")
+    def validate_name(self, key, value):
+        if not isinstance(value, str) or len(value) == 0:
+            raise ValueError(f"{key} must be string")
+        return value
+
 
 class Tutor(db.Model, SerializerMixin):
-    __tablename__ = 'tutors'
+    __tablename__ = "tutors"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     specialty = db.Column(db.String)
 
     # Add relationships
-    
+    sessions = db.relationship("Session", back_populates="tutor")
+
     # Add serialization rules
-    
+    serialize_rules = ("-sessions.tutor",)
+
     # Add validations
+    @validates("name")
+    def validate_name(self, key, value):
+        if not isinstance(value, str) or len(value) == 0:
+            raise ValueError(f"{key} must be string")
+        return value
+
+    @validates("specialty")
+    def validate_specialty(self, key, value):
+        if not isinstance(value, str) or len(value) == 0:
+            raise ValueError(f"{key} must be string")
+        return value
+
 
 class Session(db.Model, SerializerMixin):
-    __tablename__ = 'sessions'
+    __tablename__ = "sessions"
 
     id = db.Column(db.Integer, primary_key=True)
-    datetime = db.Column(db.DateTime)
+    datetime = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
     # Add relationships
-   
+    student_id = db.Column(db.Integer, db.ForeignKey("students.id"))
+    tutor_id = db.Column(db.Integer, db.ForeignKey("tutors.id"))
+    student = db.relationship("Student", back_populates="sessions")
+    tutor = db.relationship("Tutor", back_populates="sessions")
     # Add serialization rules
-    
+    serialize_rules = (
+        "-student.sessions",
+        "-tutor.sessions",
+    )
+
     # Add validations
+    @validates("datetime")
+    def validate_datetime(self, key, value):
+        if not isinstance(value, datetime.datetime):
+            raise ValueError(f"{key} must be date type")
+        return value
+
+    @validates("student_id")
+    def validate_student_id(self, key, value):
+        if not isinstance(value, int) or value < 1:
+            raise ValueError(f"{key} must be int and above 0")
+        return value
+
+    @validates("tutor_id")
+    def validate_tutor_id(self, key, value):
+        if not isinstance(value, int) or value < 1:
+            raise ValueError(f"{key} must be int and above 0")
+        return value
+
 
 # add any models you may need.
